f86d10325533332ede7dbe08617dbfecd0202f7e
nivswisa11 <swisaniv@gmail.com>
2022-12-30 14:17:30 +0200

    this is the version with the splitting to server and client

--- server/main.cpp
+++ server/main.cpp
@@ -2,8 +2,14 @@
 #include "VectorCreation.h"
 #include "VectorBase.h"
 #include "CsvReading.h"
+#include <iostream>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
 
-using namespace std;
+#include <unistd.h>
+#include <string.h>
+int calculate(char buffer[]);
 
 /**
  * This function is the main function of the program. It takes in the command line arguments and calls the other
@@ -12,7 +18,48 @@ using namespace std;
  * @param argc the number of arguments passed to the program
  * @param argv
  */
-int main(int argc, char *argv[]) {
+int main() {
+    const int server_port = 5555;
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("error creating socket"); }
+    struct sockaddr_in sin;
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(server_port);
+    if (::bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
+        perror("error binding socket"); }
+    if (listen(sock, 5) < 0) {
+        perror("error listening to a socket");
+    }
+    struct sockaddr_in client_sin;
+    unsigned int addr_len = sizeof(client_sin);
+    int client_sock = accept(sock, (struct sockaddr *) &client_sin, &addr_len);
+    if (client_sock < 0) {
+        perror("error accepting client"); }
+    char buffer[4096];
+    int expected_data_len = sizeof(buffer);
+    int read_bytes = recv(client_sock, buffer, expected_data_len, 0);
+    if (read_bytes == 0) {
+// connection is closed
+    }
+    else if (read_bytes < 0) {
+// error
+    }
+    else {
+        std::cout << "Server " << buffer; }
+
+    int sent_bytes = send(client_sock, buffer, read_bytes, 0);
+    if (sent_bytes < 0) {
+        perror("error sending to client"); }
+    close(sock);
+    calculate(buffer);
+    return 0;
+}
+
+int calculate(char buffer[]) {
+
     vector<VectorBase> masterVector;
     vector<double> inputVector;
     int k = stoi(argv[1]);
